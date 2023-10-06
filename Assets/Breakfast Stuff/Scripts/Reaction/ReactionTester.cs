63f17ea36fc57eb4e504a27dda1ae5259620387a
Alex Locke <alexhlocke@gmail.com>
2023-10-06 16:04:25 -0700

    Reaction System First Iteration!

---
+++ Assets/Breakfast Stuff/Scripts/Reaction/ReactionTester.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactionTester : MonoBehaviour
+{
+    [Header("Reaction GameObject to Test")]
+    public GameObject reactionGameObject;
+
+    private ReactionProfile reactionProfile;
+
+
+    void Start() {
+        reactionProfile = reactionGameObject.GetComponent<ReactionProfile>();
+    }
+
+    void Update() {
+        // Check for key presses to trigger reactions
+        if (Input.GetKeyDown(KeyCode.C)) {
+            // Queue the "sad" reaction for 3 seconds
+            ReactionCommand reactionCommand = new ReactionCommand(reactionProfile.happySprite, 1f);
+            reactionProfile.QueueReaction(reactionCommand);
+        } 
+        if (Input.GetKeyDown(KeyCode.X)) {
+            // Queue the "happy" reaction for 3 seconds
+            ReactionCommand reactionCommand = new ReactionCommand(reactionProfile.sadSprite, 2f);
+            reactionProfile.QueueReaction(reactionCommand);
+        }
+    }
+}
