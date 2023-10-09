3e9c1d59f7bac58cdd85e057ee29e4c9258e7ec0
blacey15 <blacey@csumb.edu>
2023-10-08 19:45:00 -0700

    timer done, score half done. still need to keep track of score and display it

---
+++ Assets/Breakfast Stuff/Scripts/Score.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (CompareTag("Apple"))
+        {
+            Debug.Log("20 points!");
+        }
+        else if (CompareTag("Tomato"))
+        {
+            Debug.Log("Minus 10 points!");
+        }
+        else if (CompareTag("Banana"))
+        {
+            Debug.Log("Minus 10 points!");
+        }
+        else if (CompareTag("Carrot"))
+        {
+            Debug.Log("20 points!");
+        }
+    }
+}
