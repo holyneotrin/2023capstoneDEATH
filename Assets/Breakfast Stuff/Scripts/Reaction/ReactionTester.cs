554e9bf2b770011141aa728bc41333f1108b2e51
Alex Locke <alexhlocke@gmail.com>
2023-10-14 16:03:07 -0700

    Audio Manager Done
    i also updated the vscode package and threw in some test sounds and whatnot

--- Assets/Breakfast Stuff/Scripts/Reaction/ReactionTester.cs
+++ Assets/Breakfast Stuff/Scripts/Reaction/ReactionTester.cs
@@ -6,6 +6,9 @@ public class ReactionTester : MonoBehaviour
 {
     [Header("Reaction GameObject to Test")]
     public GameObject reactionGameObject;
+    [Header("Audio (for testing)")]
+    public AudioClip goodSound;
+    public AudioClip badSound;
 
     private ReactionProfile reactionProfile;
 
@@ -21,24 +24,28 @@ public class ReactionTester : MonoBehaviour
             ReactionCommand reactionCommand = new ReactionCommand(reactionProfile.loveSprite, 1f);
             reactionProfile.QueueReaction(reactionCommand);
             Debug.Log("Queueing 'Love' for 1 Second");
+            AudioManager.instance.PlaySound(goodSound, 100f);
         } 
-        if (Input.GetKeyDown(KeyCode.X)) {
-            // Queue the "sad" reaction for 3 seconds
-            ReactionCommand reactionCommand = new ReactionCommand(reactionProfile.angrySprite, 1f);
-            reactionProfile.QueueReaction(reactionCommand);
-            Debug.Log("Queueing 'Angry' for 3 Seconds");
-        }
         if (Input.GetKeyDown(KeyCode.C)) {
-            // Queue the "sad" reaction for 3 seconds
+            // Queue the "Success" reaction for 3 seconds
             ReactionCommand reactionCommand = new ReactionCommand(reactionProfile.successSprite, 1f);
             reactionProfile.QueueReaction(reactionCommand);
             Debug.Log("Queueing 'Success' for 3 Seconds");
+            AudioManager.instance.PlaySound(goodSound, 100f);
+        }
+        if (Input.GetKeyDown(KeyCode.X)) {
+            // Queue the "Angry" reaction for 3 seconds
+            ReactionCommand reactionCommand = new ReactionCommand(reactionProfile.angrySprite, 1f);
+            reactionProfile.QueueReaction(reactionCommand);
+            Debug.Log("Queueing 'Angry' for 3 Seconds");
+            AudioManager.instance.PlaySound(badSound, 100f);
         }
         if (Input.GetKeyDown(KeyCode.Z)) {
-            // Queue the "sad" reaction for 3 seconds
+            // Queue the "Fail" reaction for 3 seconds
             ReactionCommand reactionCommand = new ReactionCommand(reactionProfile.failSprite, 1f);
             reactionProfile.QueueReaction(reactionCommand);
             Debug.Log("Queueing 'Fail' for 3 Seconds");
+            AudioManager.instance.PlaySound(badSound, 100f);
         }
     }
 }
