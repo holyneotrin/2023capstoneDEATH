c66dc520305905db2c16f2ea6a571ee2c9058411
brielle <blacey@csumb.edu>
2023-11-10 14:42:03 -0800

    score and timer done

--- Assets/Breakfast Stuff/Scripts/ScoreManager.cs
+++ Assets/Breakfast Stuff/Scripts/ScoreManager.cs
@@ -27,29 +27,29 @@ public class ScoreManager : MonoBehaviour
         
     }
 
-    private void OnMouseDown()
-    {
-        if (CompareTag("Apple"))
-        {
-            Debug.Log("20 points!");
-            addPoints();
-        }
-        else if (CompareTag("Tomato"))
-        {
-            Debug.Log("Minus 10 points!");
-            subtractPoints();
-        }
-        else if (CompareTag("Banana"))
-        {
-            Debug.Log("Minus 10 points!");
-            subtractPoints();
-        }
-        else if (CompareTag("Carrot"))
-        {
-            Debug.Log("20 points!");
-            addPoints();
-        }
-    }
+    // private void OnMouseDown()
+    // {
+    //     if (CompareTag("Apple"))
+    //     {
+    //         Debug.Log("20 points!");
+    //         addPoints();
+    //     }
+    //     else if (CompareTag("Tomato"))
+    //     {
+    //         Debug.Log("Minus 10 points!");
+    //         subtractPoints();
+    //     }
+    //     else if (CompareTag("Banana"))
+    //     {
+    //         Debug.Log("Minus 10 points!");
+    //         subtractPoints();
+    //     }
+    //     else if (CompareTag("Carrot"))
+    //     {
+    //         Debug.Log("20 points!");
+    //         addPoints();
+    //     }
+    // }
 
     public void addPoints()
     {
