af3b54dd43ab8fd7f0c69d74589e40088d148b84
brielle <blacey@csumb.edu>
2023-11-29 13:57:36 -0800

    ingredient mini game

--- Assets/Breakfast Stuff/Scripts/ScoreManager.cs
+++ Assets/Breakfast Stuff/Scripts/ScoreManager.cs
@@ -9,47 +9,13 @@ public class ScoreManager : MonoBehaviour
     public Text scoreText;
     private int score;
 
-    public GameObject apple;
-    public GameObject tomato;
-    public GameObject banana;
-    public GameObject carrot;
-
+  
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
         scoreText.text = "Score: " + score.ToString();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    // private void OnMouseDown()
-    // {
-    //     if (CompareTag("Apple"))
-    //     {
-    //         Debug.Log("20 points!");
-    //         addPoints();
-    //     }
-    //     else if (CompareTag("Tomato"))
-    //     {
-    //         Debug.Log("Minus 10 points!");
-    //         subtractPoints();
-    //     }
-    //     else if (CompareTag("Banana"))
-    //     {
-    //         Debug.Log("Minus 10 points!");
-    //         subtractPoints();
-    //     }
-    //     else if (CompareTag("Carrot"))
-    //     {
-    //         Debug.Log("20 points!");
-    //         addPoints();
-    //     }
-    // }
 
     public void addPoints()
     {
