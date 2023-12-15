07e36f5c2c98237410db86d3cea5f737bc4a6239
Alex Locke <alexhlocke@gmail.com>
2023-12-14 18:10:05 -0800

    Minigame Scene Swapping Works

---
+++ Assets/LoadEndLandingPage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class LoadEndLandingPage : MonoBehaviour
+{
+    public void LoadEndScreen() {
+        Debug.Log("Loading End Screen");
+        SceneManager.LoadScene("EndGameLandingPage", LoadSceneMode.Single);
+    }
+
+}
---
+++ Assets/ReturnToMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenu : MonoBehaviour
+{
+    public void LoadMenu() {
+        SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
+    }
+}
